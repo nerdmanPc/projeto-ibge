77df32dd50998922602aa70f6da42f4b53b95063
Pedro Costa <antonhyonhi@gmail.com>
2022-04-20 18:17:07 -0300

    Some parameters are missing. Missing error handling. Refactor to us functions.

--- ibge_lib.py
+++ ibge_lib.py
@@ -5,19 +5,31 @@ class IBGEQuery:
     def __init__(self) -> None:
         self._url = 'https://servicodados.ibge.gov.br/api/v3/agregados'
 
-    def filter_subject(self, code):
+    def select_subject(self, code):
         self._url += f'?assunto={code}'
 
     def select_aggregate(self, code):
         self._url += f'/{code}'
 
-    def filter_location_levels(self, levels):
+    def select_location_levels(self, levels):
         level_ids = [f'N{level}' for level in levels]
         suffix = '|'.join(level_ids)
         self._url += f'/localidades/{suffix}'
 
     def filter_metadata(self):
         self._url += '/metadados'
+
+    def filter_periods(self):
+        self._url += '/periodos'
+
+    def select_periods(self, periods):
+        self._url += f'/{periods}'
+
+    def filter_variables(self):
+        self._url += '/variaveis'
+
+    def select_locations(self, locations):
+        self._url += f'?localidades={locations}'
 
     def url(self):
         return self._url
--- ibge_test.py
+++ ibge_test.py
@@ -12,17 +12,32 @@ class TestIBGE(unittest.TestCase):
 
     def test_filtered_aggregate_query(self):
         query = IBGEQuery()
-        query.filter_subject(70)
+        query.select_subject(70)
         assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados?assunto=70'
 
     def test_locations_per_aggregate_query(self):
         query = IBGEQuery()
         query.select_aggregate(1705)
-        query.filter_location_levels([7, 6])
+        query.select_location_levels([7, 6])
         assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/localidades/N7|N6'
 
-    def test_aggregate_metadata_query(self):
+    def test_metadata_per_aggregate_query(self):
         query = IBGEQuery()
         query.select_aggregate(1705)
         query.filter_metadata()
         assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/metadados'
+
+    def test_periods_per_aggregate_query(self):
+        query = IBGEQuery()
+        query.select_aggregate(1705)
+        query.filter_periods()
+        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos'
+
+    def test_variable_query(self):
+        query = IBGEQuery()
+        query.select_aggregate(1705)
+        query.filter_periods()
+        query.select_periods(-6)
+        query.filter_variables()
+        query.select_locations('BR')
+        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos/-6/variaveis?localidades=BR'
