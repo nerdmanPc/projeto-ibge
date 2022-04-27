fe09dffb30034287d8005712124a0bfb4485450c
Pedro Costa <antonhyonhi@gmail.com>
2022-04-27 16:30:27 -0300

    Refactored IBGE's API.

--- ibge_lib.py
+++ ibge_lib.py
@@ -1,39 +1,6 @@
 import urllib3
 import json
 
-class IBGEQuery:
-    def __init__(self) -> None:
-        self._url = 'https://servicodados.ibge.gov.br/api/v3/agregados'
-
-    def select_subject(self, code):
-        self._url += f'?assunto={code}'
-
-    def select_aggregate(self, code):
-        self._url += f'/{code}'
-
-    def select_location_levels(self, levels):
-        level_ids = [f'N{level}' for level in levels]
-        suffix = '|'.join(level_ids)
-        self._url += f'/localidades/{suffix}'
-
-    def filter_metadata(self):
-        self._url += '/metadados'
-
-    def filter_periods(self):
-        self._url += '/periodos'
-
-    def select_periods(self, periods):
-        self._url += f'/{periods}'
-
-    def filter_variables(self):
-        self._url += '/variaveis'
-
-    def select_locations(self, locations):
-        self._url += f'?localidades={locations}'
-
-    def url(self):
-        return self._url
-
 def query_ibge(aggregate, variables, classification, periods, locales, http=None):
     query_url = f'https://servicodados.ibge.gov.br/api/v3/agregados/{aggregate}/periodos/{periods}/variaveis/{variables}?localidades={locales}&classificacao={classification}'
     response = http.request('GET', query_url)
@@ -42,3 +9,24 @@ def query_ibge(aggregate, variables, classification, periods, locales, http=None
         raise
     response_str = response.data.decode('utf-8')
     return json.loads(response_str)[0]['resultados']
+
+AGGR_URL = 'https://servicodados.ibge.gov.br/api/v3/agregados'
+
+def aggregates_url(subject=None):
+    if subject is None:
+        return AGGR_URL
+    else:
+        return AGGR_URL + f'?assunto={subject}'
+
+def locations_url(aggregate, levels):
+    levels_str = '|'.join(levels)
+    return AGGR_URL + f'/{aggregate}/localidades/{levels_str}'
+
+def metadata_url(aggregate):
+    return AGGR_URL + f'/{aggregate}/metadados'
+
+def periods_url(aggregate):
+    return AGGR_URL + f'/{aggregate}/periodos'
+
+def variables_url(aggregate, periods='-6', locations='BR'):
+    return AGGR_URL + f'/{aggregate}/periodos/{periods}/variaveis?localidades={locations}'
--- ibge_test.py
+++ ibge_test.py
@@ -7,37 +7,19 @@ from ibge_lib import *
 
 class TestIBGE(unittest.TestCase):
     def test_unfiltered_aggergate_query(self):
-        query = IBGEQuery()
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados'
+        assert aggregates_url() == 'https://servicodados.ibge.gov.br/api/v3/agregados'
 
     def test_filtered_aggregate_query(self):
-        query = IBGEQuery()
-        query.select_subject(70)
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados?assunto=70'
+        assert aggregates_url(subject=70) == 'https://servicodados.ibge.gov.br/api/v3/agregados?assunto=70'
 
     def test_locations_per_aggregate_query(self):
-        query = IBGEQuery()
-        query.select_aggregate(1705)
-        query.select_location_levels([7, 6])
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/localidades/N7|N6'
+        assert locations_url(aggregate=1705, levels=['N7', 'N6']) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/localidades/N7|N6'
 
     def test_metadata_per_aggregate_query(self):
-        query = IBGEQuery()
-        query.select_aggregate(1705)
-        query.filter_metadata()
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/metadados'
+        assert metadata_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/metadados'
 
     def test_periods_per_aggregate_query(self):
-        query = IBGEQuery()
-        query.select_aggregate(1705)
-        query.filter_periods()
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos'
+        assert periods_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos'
 
     def test_variable_query(self):
-        query = IBGEQuery()
-        query.select_aggregate(1705)
-        query.filter_periods()
-        query.select_periods(-6)
-        query.filter_variables()
-        query.select_locations('BR')
-        assert query.url() == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos/-6/variaveis?localidades=BR'
+        assert variables_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos/-6/variaveis?localidades=BR'
