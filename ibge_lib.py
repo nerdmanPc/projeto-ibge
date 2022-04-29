90f3d9f1f26c99d73f699d520dd38545a2cb6f4d
Pedro Costa <antonhyonhi@gmail.com>
2022-04-29 18:44:40 -0300

    ibge_lib is not complete, but it's functional.

--- ibge_lib.py
+++ ibge_lib.py
@@ -1,14 +1,11 @@
 import urllib3
 import json
 
-def query_ibge(aggregate, variables, classification, periods, locales, http=None):
-    query_url = f'https://servicodados.ibge.gov.br/api/v3/agregados/{aggregate}/periodos/{periods}/variaveis/{variables}?localidades={locales}&classificacao={classification}'
-    response = http.request('GET', query_url)
+def query_ibge(query_url, request_fn):
+    response = request_fn(query_url)
     if response.status != 200:
-        print(f'ERRO: {response.status}')
-        raise
-    response_str = response.data.decode('utf-8')
-    return json.loads(response_str)[0]['resultados']
+        raise f'ERRO {response.status}'
+    return json.loads(response.data)
 
 AGGR_URL = 'https://servicodados.ibge.gov.br/api/v3/agregados'
 
@@ -28,5 +25,28 @@ def metadata_url(aggregate):
 def periods_url(aggregate):
     return AGGR_URL + f'/{aggregate}/periodos'
 
-def variables_url(aggregate, periods='-6', locations='BR'):
-    return AGGR_URL + f'/{aggregate}/periodos/{periods}/variaveis?localidades={locations}'
+def all_variables_url(aggregate, locations='BR'):
+    return AGGR_URL + f'/{aggregate}/variaveis/all?localidades={locations}'
+
+def specific_variables_url(aggregate, variables, locations=['BR'], periods=['-6'], classifications=None):
+    periods_str = '|'.join(periods)
+    variables_str = '|'.join(variables)
+    locations_str = '|'.join(locations)
+    classifications_fmt = ''
+    if not(classifications is None):
+        classifications_str = '|'.join(classifications)
+        classifications_fmt = f'&classificacao={classifications_str}'
+    return AGGR_URL + f'/{aggregate}/periodos/{periods_str}/variaveis/{variables_str}?localidades={locations_str}{classifications_fmt}'
+
+def filter_researches(reseaches, keyword: str):
+    filter_fn = lambda x: x['nome'].casefold().rfind(keyword.casefold()) != -1
+    filter_it = filter(filter_fn, reseaches)
+    return list(filter_it)
+
+def filter_aggregates(researches, keyword: str):
+    aggregates = []
+    for research in researches:
+        aggregates.extend(research['agregados'])
+    filter_fn = lambda x: x['nome'].casefold().rfind(keyword.casefold()) != -1
+    filter_it = filter(filter_fn, aggregates)
+    return list(filter_it)
--- ibge_test.py
+++ ibge_test.py
@@ -5,21 +5,44 @@ import json
 
 from ibge_lib import *
 
-class TestIBGE(unittest.TestCase):
+class TestIBGEQueries(unittest.TestCase):
     def test_unfiltered_aggergate_query(self):
-        assert aggregates_url() == 'https://servicodados.ibge.gov.br/api/v3/agregados'
+        self.assertEqual(aggregates_url(), 'https://servicodados.ibge.gov.br/api/v3/agregados')
 
     def test_filtered_aggregate_query(self):
-        assert aggregates_url(subject=70) == 'https://servicodados.ibge.gov.br/api/v3/agregados?assunto=70'
+        self.assertEqual(aggregates_url(subject=70), 'https://servicodados.ibge.gov.br/api/v3/agregados?assunto=70')
 
     def test_locations_per_aggregate_query(self):
-        assert locations_url(aggregate=1705, levels=['N7', 'N6']) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/localidades/N7|N6'
+        self.assertEqual(locations_url(aggregate=1705, levels=['N7', 'N6']), 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/localidades/N7|N6')
 
     def test_metadata_per_aggregate_query(self):
-        assert metadata_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/metadados'
+        self.assertEqual(metadata_url(aggregate=1705), 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/metadados')
 
     def test_periods_per_aggregate_query(self):
-        assert periods_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos'
+        self.assertEqual(periods_url(aggregate=1705), 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos')
 
-    def test_variable_query(self):
-        assert variables_url(aggregate=1705) == 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/periodos/-6/variaveis?localidades=BR'
+    def test_all_variables_query(self):
+        self.assertEqual(all_variables_url(aggregate=1705), 'https://servicodados.ibge.gov.br/api/v3/agregados/1705/variaveis/all?localidades=BR')
+
+    def test_specific_variables_query(self):
+        query_url = specific_variables_url (aggregate=1712, locations=['BR'], variables=['214', '1982'], periods=['201701-201706', '201710'])
+        self.assertEqual(query_url, 'https://servicodados.ibge.gov.br/api/v3/agregados/1712/periodos/201701-201706|201710/variaveis/214|1982?localidades=BR')
+
+    def test_variables_query_with_classification(self):
+        query_url = specific_variables_url(aggregate=1712, locations=['BR'], variables=['214', '1982'], classifications=['226[4844,96608,96609]', '218[4780]'])
+        self.assertEqual(query_url, 'https://servicodados.ibge.gov.br/api/v3/agregados/1712/periodos/-6/variaveis/214|1982?localidades=BR&classificacao=226[4844,96608,96609]|218[4780]')
+
+class TestIBGEResponses(unittest.TestCase):
+    def setUp(self):
+        with open('aggregates_response_test.json', 'rb') as file:
+            self._aggregates = json.load(file)
+        with open('variables_response_test.json', 'rb') as file:
+            self._variables = json.load(file)
+    
+    def test_filter_research(self):
+        researches_by_keyword = filter_researches(self._aggregates, 'cenoura')
+        self.assertEqual(researches_by_keyword[0]['id'], 'P2')
+
+    def test_filter_aggregate(self):
+        aggregates_by_keyword = filter_aggregates(self._aggregates, 'melao')
+        self.assertEqual(aggregates_by_keyword[0]['id'], 200)
